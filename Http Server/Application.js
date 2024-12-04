49737f69556f475185a9277f94067723d87a6938
innovativenavneet <navneet.prakash1233@gmail.com>
2024-12-04 16:36:30 +0530

    HTTP REQUEST COMMITED

--- Http Server/Application.js
+++ Http Server/Application.js
@@ -1,9 +1,39 @@
 const http = require("http");
 const fs = require("fs");
+// initializin url to pass 
+const url = require("url");
+
+
 
 // Create a server to handle requests
 const myServer = http.createServer((req, res) => {
   const log = `${Date.now()} : ${req.url} New request received\n`;
+
+
+
+    // THIS IS HOW WE HANDLE QUERY IN NODE JS 
+
+
+
+//   lets start the parsing of the URL 
+  const myUrl = url.parse(req.url,true); //ths will let myUrl to save the path name  
+  console.log(myUrl);
+//   this will return :: 
+// Url {
+//     protocol: null,
+//     slashes: null,
+//     auth: null,
+//     host: null,
+//     port: null,
+//     hostname: null,
+//     hash: null,
+//     search: '?=0',
+//     query: '=0',
+//     pathname: '/about/navneet',
+//     path: '/about/navneet?=0',
+//     href: '/about/navneet?=0'
+//   }
+
 
   // Append the log to 'log.txt'
   fs.appendFile("log.txt", log, (err) => {
@@ -15,15 +45,22 @@ const myServer = http.createServer((req, res) => {
     }
 
     // Log file written successfully, now handle the request
-    switch (req.url) {
+
+    // switch (req.url) =< insted of this we will use myUrl.pathname
+    switch (myUrl.pathname) {
       case '/': // Homepage
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Homepage");
         break;
 
       case '/about': // About page
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.end("About");
+
+      const username = myUrl.query.myname; //this will store the query name that will pass by the user 
+      res.writeHead(200, { "Content-Type": "text/plain" });
+  
+    //   this will throw this username to interface of the user 
+      res.end(`Hi, ${username}`);
+
         break;
 
       case '/contacts': // Contacts page
